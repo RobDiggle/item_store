b806b2b6d35c07b689b849da1e58c6a6c3a9b0b1
Robert Diggle <Robschach13@yahoo.com>
2026-02-23 16:31:50 +0700

    2/23/2026 Afternoon commits - Made items and button clicks actually align.

--- buy_items.py
+++ buy_items.py
@@ -9,6 +9,7 @@ class store_item:
 # if you cut out the next  line then local variable message is not associated with a value
 
 items = [
+# Office & Home goods section
      store_item("Book", 100, 12, True),
      store_item("Desk", 100, 40, True),
      store_item("Coffee mug", 100, 10, True),
@@ -19,10 +20,10 @@ items = [
      store_item("Fruit", 10, 1, True),
      store_item("Milk", 10, 2, True),
 # Tech section
-     store_item("Mouse", 20, 1, True),
-     store_item("Charger", 20, 4, True),
-     store_item("Router", 100, 1, True),
-     store_item("Laptop", 20, 100, True),
+     store_item("Mouse", 1, 20, True),
+     store_item("Charger", 2, 30, True),
+     store_item("Router", 2, 85, True),
+     store_item("Laptop", 2, 100, True),
 
         ]
 
